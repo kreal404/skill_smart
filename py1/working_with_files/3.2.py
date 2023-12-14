2d2f044ad3a529c4c94280229a4654522f832688
kirill.sundaen <jws-89@mail.ru>
2023-12-14 12:47:34 +0300

    Modified file

--- py1/working_with_files/3.2.py
+++ py1/working_with_files/3.2.py
@@ -1,9 +1,9 @@
 import random
 
-def sum_two_numbers(n1, n2):
+def sum_two_numbers(path, n1, n2):
     try:
-        with open(f"./{n1}.txt") as file1, \
-             open(f"./{n2}.txt") as file2:
+        with open(f"{path}/{n1}.txt") as file1, \
+             open(f"{path}/{n2}.txt") as file2:
 
             nums1 = []
             for line in file1:
@@ -31,8 +31,10 @@ def sum_two_numbers(n1, n2):
         if file2:
             file2.close()
 
+path = "."
+
 n1 = random.randint(1, 10)
 n2 = random.randint(1, 10)
 
-result = sum_two_numbers(n1, n2)
+result = sum_two_numbers(path, n1, n2)
 print(f"Сумма чисел из файлов {n1}.txt и {n2}.txt: {result}")
