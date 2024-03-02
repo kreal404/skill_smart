f8cd8081cae993ca446aaac11e1e66c942f072a1
kirill.sundaen <jws-89@mail.ru>
2024-03-02 21:46:54 +0300

    first task

--- 28/1.py
+++ 28/1.py
@@ -1,8 +1,8 @@
-def squirrel(N):
-    factorial = 1
+def squirrel(N: int) -> int:
+    factorial: int = 1
     while N > 1:
         factorial *= N
         N -= 1
 
-    first_digit = int(str(factorial)[0])
+    first_digit: int = int(str(factorial)[0])
     return first_digit
