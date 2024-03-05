8ac77333907df17e43ecf862c2b8cedd80a8a3f7
kirill.sundaen <jws-89@mail.ru>
2024-03-06 01:21:23 +0300

    third task

--- 28/3.py
+++ 28/3.py
@@ -1,35 +1,33 @@
 def ConquestCampaign(N, M, L, battalion):
-    field = []
+    field = [[0] * M for _ in range(N)]
     day = 1
     cell = 0
 
-    for i in range(N):
-        field.append([0]*M)
-    for j in range(0, len(battalion), 2):
-        field[battalion[j]-1] [battalion[j+1]-1] = 1
-        cell += 1
-    day += 1
-    
-    while cell != N*M:
-        for r in range(len(field)):
-            for c in range(len(field[r])):
+    for i in range(0, len(battalion), 2):
+        row = battalion[i] - 1
+        col = battalion[i+1] - 1
+        if field[row][col] == 0:
+            field[row][col] = 1
+            cell += 1
+
+    while cell != N * M:
+        for r in range(N):
+            for c in range(M):
                 if field[r][c] == 1:
-                    if c-1 >= 0 and field[r][c-1] == 0:
-                        field[r][c-1] = 1
+                    if c - 1 >= 0 and field[r][c - 1] == 0:
+                        field[r][c - 1] = 1
                         cell += 1
-                    if c+1 < M and field[r][c+1] == 0:
-                        field[r][c+1] = 1
+                    if c + 1 < M and field[r][c + 1] == 0:
+                        field[r][c + 1] = 1
                         cell += 1
-                    if r-1 >= 0 and field[r-1][c] == 0:
-                        field[r-1][c] = 1
+                    if r - 1 >= 0 and field[r - 1][c] == 0:
+                        field[r - 1][c] = 1
                         cell += 1
-                    if r+1 < N and field[r+1][c] == 0:
-                        field[r+1][c] = 1
+                    if r + 1 < N and field[r + 1][c] == 0:
+                        field[r + 1][c] = 1
                         cell += 1
-        if cell == M*N:
-            break
+
         day += 1
 
     return day
-
 
