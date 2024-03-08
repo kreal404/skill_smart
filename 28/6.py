b19d9dd096bb8ef07cffbfe46ee9222f6fc22d3a
kirill.sundaen <jws-89@mail.ru>
2024-03-08 18:25:34 +0300

    6th_task

--- 28/6.py
+++ 28/6.py
@@ -4,10 +4,11 @@ def PatternUnlock (N, hits):
     for i in range(1, N):
         b = a[hits[i-1]-1]
         c = a[hits[i]-1]
-        counter += ((c[1]-b[1])2 + (b[0]-c[0])2)**0.5
+        counter += ((c[1]-b[1])**2 + (b[0]-c[0])**2)**0.5
     cn = round(counter, 5)
     s = str(cn)
     s = s.replace('0','')
     s = s.replace('.','')
     return s
+
 
