e4348fa8525a715ab919623ecfe26249b34db7c8
kirill.sundaen <jws-89@mail.ru>
2024-03-10 18:06:02 +0300

    7th task

--- 28/7.py
+++ 28/7.py
@@ -1,24 +1,28 @@
 def WordSearch(max_len: int, s: str, subs: str) -> str:
     result = []
-    
+
     lines = s.split("\n")
     for line in lines:
         words = line.split()
         new_lines = []
         current_line = ""
-        
-        for word in words:
-            if len(current_line) + len(word) <= max_len:
-                current_line += word + " "
-            else:
-                new_lines.append(current_line.strip())
-                current_line = word + " "
-        
-        new_lines.append(current_line.strip())
-        
-        for new_line in new_lines:
-            result.append(str(int(subs in new_line)))
-    
-    return " ".join(result)
 
+        for word in words:
+            if len(current_line) + len(word) > max_len:
+                if current_line:
+                    new_lines.append(current_line.strip())
+                    current_line = ""
+            current_line += word + " "
+            if len(current_line) - 1 == max_len:
+                new_lines.append(current_line.strip())
+                current_line = ""
+
+        if current_line:
+            new_lines.append(current_line.strip())
+
+        for new_line in new_lines:
+            words_in_line = new_line.split()
+            result.append(str(int(subs in words_in_line)))
+
+    return " ".join(result)
 
