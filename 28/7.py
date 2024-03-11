b5aa55a7bf973e60868288f1438e2cc201f03090
kirill.sundaen <jws-89@mail.ru>
2024-03-11 18:05:55 +0300

    7th task

--- 28/7.py
+++ 28/7.py
@@ -6,7 +6,13 @@ def WordSearch(line_length: int, s: str, subs: str) -> list[int]:
     current_line = ""
 
     for word in words:
-        if len(current_line) + len(word) + 1 <= line_length:
+        if len(word) > line_length:
+            if current_line:
+                new_lines.append(current_line)
+            for i in range(0, len(word), line_length):
+                new_lines.append(word[i:i+line_length])
+            current_line = ""
+        elif len(current_line) + len(word) + 1 <= line_length:
             if current_line:
                 current_line += " "
             current_line += word
@@ -14,13 +20,13 @@ def WordSearch(line_length: int, s: str, subs: str) -> list[int]:
             new_lines.append(current_line)
             current_line = word
 
-    new_lines.append(current_line)
+    if current_line:
+        new_lines.append(current_line)
 
     for new_line in new_lines:
         result.append(int(subs in new_line.strip()))
 
     return result
-
 
 
 
