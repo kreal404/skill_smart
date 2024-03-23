16787b10bb4b81802f02b28b7290b153093d9c19
kirill.sundaen <jws-89@mail.ru>
2024-03-23 16:34:47 +0300

    14th task

---
+++ 28/14.py
@@ -0,0 +1,20 @@
+def Unmanned(L, N, track):
+    time = 0
+    last_pos = 0
+
+    for traffic_light_info in track:
+        pos, red, green = traffic_light_info
+
+        time += pos - last_pos
+
+        traffic_light = time % (red + green)
+
+        if traffic_light < red:
+            time += red - traffic_light
+
+        last_pos = pos
+
+    time += L - last_pos
+
+    return time
+
