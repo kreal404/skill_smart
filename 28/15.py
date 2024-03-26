b33c4a7663e182a20b26c825cd952d74c5139623
kirill.sundaen <jws-89@mail.ru>
2024-03-26 19:28:13 +0300

    15th task

--- 28/15.py
+++ 28/15.py
@@ -1,13 +1,30 @@
-def TankRush(H1: int, W1: int, S1: str, H12: int, W2: int, S2: str) -> bool:
-    total_map = S1.split()
-    tanks_map = S2.split()
-    count = 0
+def string_to_list(s: str) -> list:
+    result = []
 
-    for i in tanks_map:
-        for j in total_map:
-            if i in j:
-                count += 1
-                break
+    for row in s.split():
+        temp = []
+        for n in row:
+            temp.append(int(n))
+        result.append(temp)
 
-    return count == len(tanks_map)
+    return result
+
+
+def TankRush(H1: int, W1: int, S1: str, H2: int, W2: int, S2: str) -> bool:
+    total_map = string_to_list(S1)
+    tanks_map = string_to_list(S2)
+
+    for i in range(H1 - H2 + 1):
+        for j in range(W1 - W2 + 1):
+            count = 0
+            
+            for x in range(H2):
+                for y in range(W2):
+                    count += total_map[i + x][j + y] == tanks_map[x][y]
+
+            if count == H2 + W2:
+                return True
+
+    return False
+
 
