57c559819972440b3c5e1a5a12e3e9ace9f5dbf0
kirill.sundaen <jws-89@mail.ru>
2024-04-07 12:36:31 +0300

    22nd task

---
+++ 28/22.py
@@ -0,0 +1,16 @@
+def sherlock_valid_string(s: str) -> bool:
+    char_frequencies = {}
+    for char in set(s):
+        char_frequencies[char] = s.count(char)
+
+    frequencies = sorted(list(char_frequencies.values()))
+    max_freq = max(frequencies, key=frequencies.count)
+
+    diff = 0
+
+    for f in frequencies:
+        diff += abs(f - max_freq)
+
+    return diff <= 1
+
+
