344496e0d2bbdea246ee856bfbe008716d622aec
kirill.sundaen <jws-89@mail.ru>
2024-04-11 12:34:21 +0300

    fix 27th task

--- 28/27.py
+++ 28/27.py
@@ -2,10 +2,8 @@ def Football(F, N):
     sorted_F = sorted(F)
     diff_indices = [i for i in range(N) if F[i] != sorted_F[i]]
 
-    if len(diff_indices) == 2:
+    if len(diff_indices) == 2 or F[::-1] == sorted_F:
         return True
-    if len(diff_indices) > 2:
-        return F == sorted_F
 
     return False
 
