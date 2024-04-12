30984913dafba3e0d31a7b97bd6c4066cf426b95
kirill.sundaen <jws-89@mail.ru>
2024-04-12 17:36:24 +0300

    fix 27th task

--- 28/27.py
+++ 28/27.py
@@ -1,9 +1,27 @@
-def Football(F, N):
-    sorted_F = sorted(F)
-    diff_indices = [i for i in range(N) if F[i] != sorted_F[i]]
+def is_sorted(arr):
+    return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
 
-    if len(diff_indices) == 2 or F[::-1] == sorted_F:
+def Football(F, N):
+    if is_sorted(F):
         return True
+
+    for i in range(N):
+        for j in range(i+1, N):
+            F[i], F[j] = F[j], F[i]
+            if is_sorted(F):
+                return True
+            F[i], F[j] = F[j], F[i]
+
+    for i in range(N):
+        for j in range(i+1, N):
+            if F[i] > F[j]:
+                start = i
+                while start < N-1 and F[start] > F[start+1]:
+                    start += 1
+                F[i:start+1] = reversed(F[i:start+1])
+                if is_sorted(F):
+                    return True
+                F[i:start+1] = reversed(F[i:start+1])
 
     return False
 
