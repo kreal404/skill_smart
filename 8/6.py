ac95703fdfac246d610af8fd7c40ded7fcacc64f
kirill.sundaen <jws-89@mail.ru>
2024-05-08 21:05:21 +0300

    separate function

--- 8/6.py
+++ 8/6.py
@@ -1,4 +1,8 @@
-def print_even_index(lst: list[int], i: int) -> None:
-    if i < len(lst):
-        print(lst[i])
-        print_even_index(lst, i + 2)
+def print_even_index(lst: list[int]) -> None:
+    helper(lst, 0)
+
+def helper(lst: list[int], index: int) -> None:
+    if index < len(lst):
+        print(lst[index])
+        helper(lst, index + 2)
+
