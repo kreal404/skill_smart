b600e383d8c11de0b82dd8752269d2596f8a9fa9
kirill.sundaen <jws-89@mail.ru>
2024-05-08 20:49:15 +0300

    separate function

--- 8/4.py
+++ 8/4.py
@@ -1,8 +1,11 @@
 def is_palindrome(s):
-    def helper(start, end):
-        if start >= end:
-            return True
-        if s[start] != s[end]:
-            return False
-        return helper(start + 1, end - 1)
-    return helper(0, len(s) - 1)
+    s = s.lower()
+    return helper(s, 0, len(s) - 1)
+
+def helper(s, start, end):
+    if start >= end:
+        return True
+    if s[start] != s[end]:
+        return False
+    return helper(s, start + 1, end - 1)
+
