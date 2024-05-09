3ee9586175e8027ea852370af7a9b0ad82462e23
kirill.sundaen <jws-89@mail.ru>
2024-05-09 19:06:00 +0300

    fix function

--- 8/6.py
+++ 8/6.py
@@ -2,7 +2,9 @@ def print_even_index(lst: list[int]) -> None:
     helper(lst, 0)
 
 def helper(lst: list[int], index: int) -> None:
-    if index < len(lst):
-        print(lst[index])
-        helper(lst, index + 2)
+    if index >= len(lst):
+        return
+    print(lst[index])
+    helper(lst, index + 2)
+
 
